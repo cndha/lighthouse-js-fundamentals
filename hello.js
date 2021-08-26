312f32bb42857a5bfdbb0d18a0ddff6ae9121004
Cindy H <cindyhaa@gmail.com>
2021-08-26 01:33:40 +0000

    hello

--- hello.js
+++ hello.js
@@ -3,3 +3,9 @@ const sayHello = function(name) {
   console.log('Hello, ' + name);
 }
 sayHello('Fernanda');
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
