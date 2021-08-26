ad679d26f1f6231a6fe77967c261dcc5144dfaf3
Cindy H <cindyhaa@gmail.com>
2021-08-26 01:45:33 +0000

    whichSchool update

--- conditionals.js
+++ conditionals.js
@@ -48,12 +48,17 @@ if (!raining) {
 
 const whichSchool  = function (age) {
   if (age < 13) {
-    whichSchool === 'Elementary school';
-  } else if (age <= 18 && age >= 13) {
-    whichSchool === 'Secondary school';
-  } else {
-    whichSchool === 'Lighthouse Labs';
+    return 'Elementary School';
+  } else if (age < 18 && age >= 13) {
+    return 'Secondary School';
+  } else if (age <= 100 && age >= 19) {
+    return 'Lighthouse Labs';
   }
 } 
-console.log(whichSchool);
+console.log('I am 35. Which school should I go to?');
+console.log(whichSchool(35));
+console.log('I am 8. Which school should I go to?');
+console.log(whichSchool(8));
+console.log('I am 14. Which school should I go to?');
+console.log(whichSchool(14));
 
