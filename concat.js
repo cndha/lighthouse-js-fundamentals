44fbe86db02fa5c68a079200515357f7d12576a8
Cindy H <cindyhaa@gmail.com>
2021-08-28 21:52:39 +0000

    looked up concat on MDN

---
+++ concat.js
@@ -0,0 +1,7 @@
+function concat(array1, array2) {
+  var output = array1.concat(array2);
+  return output;
+}
+console.log(concat([1, 2, 3], [4, 5, 6]));
+
+// legit just copied the .concat property from MDN 
